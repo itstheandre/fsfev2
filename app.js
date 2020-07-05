74077a37fcc93bc66fbb0c54faa2d5eb40ad3016
Andre <itstheandre@gmail.com>
2020-07-05 13:25:38 +0200

    Teapot

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express = require("express")
+const express = require("express");
 
 const port = 3000;
 
-const app = express()
+const app = express();
 
-app.get("/", ()=> {
-	res.send("Hello World");
-})
+app.get("/", () => {
+  res.send("Hello World");
+});
 
-app.listen(port, ()=> {
-	console.log(`App Running on Port ${port}`)
-})
+app.get("/demo", (req, res) => {
+  res.set("X-full0stacl", "4life");
+  res.status(418);
+  res.send("I prefer coffee");
+});
+
+app.listen(port, () => {
+  console.log(`App Running on Port ${port}`);
+});
